38c01e380c3aa480ec6de3a3e4db5ba0c8ea6ce1
Corey Sawin <sawincp@gmail.com>
2022-11-27 18:54:27 -0500

    commit tree 619939049d97d3b64e73c027c35027d5e9a137a6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+let drivers =['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(value){
+        return value * multiplier;
+    }
+}
+
+const fareDoubler = function(createFareMultiplier){
+    return createFareMultiplier * 2;
+}
+
+const fareTripler = function(createFareMultiplier){
+    return createFareMultiplier * 3;
+}
+
+function selectDifferentDrivers(drivers, fn){
+    return fn(drivers);
+}
